86f4eabbfcab5ae1b42d388149d46d44b45df9e7
SakakibaraKai <115040382+SakakibaraKai@users.noreply.github.com>
2024-03-21 23:57:55 -0700

    Update favorites.jsx
    push

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -3,6 +3,17 @@ import { useSearchParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { css } from '@emotion/react'
 
-export default function favorates() {
-    
+export default function favorates(items) {
+    const {name} = items;
+    const dispatch = useDispatch();
+
+    const removeFromFavorites = () => {
+        dispatch(removeFromFavorites(name));
+    }
+
+    const handleaddToFavorites = () => {
+        dispatch(addToFavorites(name));
+    }
+
+
 }
